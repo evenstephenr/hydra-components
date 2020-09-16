3a0027f7948ee03da3d81b868ec4eef5d95f1f20
evenstephenr <github@ramirs.com>
2020-09-16 09:35:30 -0400

    Components(src):init as a react app, add sample Button component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from './components';
+
+export const App = () => (
+  <>
+    <div>TODO</div>
+    <Button onClick={() => alert('sup')}>sup</Button>
+  </>
+)
---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Button = (props) => {
+  const { children, ...rest } = props;
+  return (
+    <button {...rest}>
+      {children}
+    </button>
+  );
+}
