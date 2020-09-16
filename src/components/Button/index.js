91cadbcd17c17df647502b4301c55030f8f5a520
evenstephenr <github@ramirs.com>
2020-09-16 11:47:02 -0400

    Components:attempt to fix broken lib exports, add log inside button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Button = (props) => {
   const { children, ...rest } = props;
+  console.log('hello from hydra/packages/components')
   return (
     <button {...rest}>
       {children}
